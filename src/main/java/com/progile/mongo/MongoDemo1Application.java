5329f929ef294a6eb756a7b2067aa986eec1f0a0
meelis <meelis.kivimae@gmail.com>
2022-07-05 13:00:19 +0300

    NON-Null fields are not exposed anymore thanks to @JsonInclude(Include.NON_NULL) Existing records are deleted and the new ones are inserted on the program startup

--- src/main/java/com/progile/mongo/MongoDemo1Application.java
+++ src/main/java/com/progile/mongo/MongoDemo1Application.java
@@ -24,16 +24,20 @@ public class MongoDemo1Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		employeeRepository.deleteAll();
+
 		List<Employee> listOfEmployees = new ArrayList<Employee>();
 		Employee e1 = new Employee("BAT", "MAN", new Address("Bat Cave", "My Back Yard", "Gotham", "CA", 1234), 5000);
 		Employee e2 = new Employee("SUPER", "MAN", new Address("Kriptonian Home", "Kriptonian City", "Kriptonian", "IN SPACE", 9999), 2000);
 		Employee e3 = new Employee("AQUA", "MAN", new Address("My House", "In Ocean", "Atlantis", "NOWHERE", 77777), 10000);
 		Employee e4 = new Employee("IRON", "MAN", new Address("Malibu", "Near Ocean", "CA", "CA", 1234), 12000);
+		Employee e5 = new Employee("PROFIT", "MAN", null, 6000);
 
 		listOfEmployees.add(e1);
 		listOfEmployees.add(e2);
 		listOfEmployees.add(e3);
 		listOfEmployees.add(e4);
+		listOfEmployees.add(e5);
 
 		employeeRepository.insert(listOfEmployees);
 
--- src/main/java/com/progile/mongo/model/Employee.java
+++ src/main/java/com/progile/mongo/model/Employee.java
@@ -2,12 +2,14 @@ package com.progile.mongo.model;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.*;
 
 @Getter
 @Setter
@@ -19,9 +21,9 @@ public class Employee {
     private String id;
     private String firstName;
     private String lastName;
+    @JsonInclude(Include.NON_NULL)
     private Address address;
     private int salary;
-
 
     public Employee(String firstName, String lastName, Address address, int salary) {
         this.firstName = firstName;
@@ -29,5 +31,4 @@ public class Employee {
         this.address = address;
         this.salary = salary;
     }
-
 }
